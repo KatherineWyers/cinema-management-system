4f1ed66dfc87ea45d5fc5092aca25d75289b90af
Katherine Wyers <contact@katherinewyers.com>
2019-07-27 08:04:57 +0700

    Add second manifest file for choice of GUI or CLI version

---
+++ LauncherCli.java
@@ -0,0 +1,22 @@
+
+/**
+ * Launch the application.
+ *
+ * @author Katherine Wyers
+ * @version 1.0
+ */
+public class LauncherCli
+{
+    public static void main(String[] args)
+    {
+        /**
+         * The default user interface is the Java-swing GUI.
+         *
+         * To launch the application using the command-line interface, comment out
+         * line 18, uncomment line 19 and compile.
+         */
+        //Gui app = new Gui();
+        Cli app = new Cli();
+        app.run();
+    }
+}
--- LauncherGui.java
+++ LauncherGui.java
@@ -5,15 +5,15 @@
  * @author Katherine Wyers
  * @version 1.0
  */
-public class Launcher
+public class LauncherGui
 {
     public static void main(String[] args)
     {
         /**
-         * The default user interface is the Java-swing GUI. 
-         * 
-         * To launch the application using the command-line interface, comment out 
-         * line 18, uncomment line 19 and compile. 
+         * The default user interface is the Java-swing GUI.
+         *
+         * To launch the application using the command-line interface, comment out
+         * line 18, uncomment line 19 and compile.
          */
         Gui app = new Gui();
         //Cli app = new Cli();
